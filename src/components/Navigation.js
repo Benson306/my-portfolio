68e777e8677f5b59b4a33c30c5fe964a8ec9394a
Benson Ndiwa <bnkimtai@gmail.com>
2023-01-19 23:04:06 +0300

    Navigation styling Done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -21,15 +21,17 @@ const Navigation = () => {
     return ( <div>
         <ul class="flex font-mono pt-5 px-10 pb-5 md:p-10 ">
             <div class="flex w-full md:basis-1/2 items-center justify-between">
-                <li>BEN</li>
-                <li class='md:invisible'><MenuOutlinedIcon fontSize={'large'} onClick={ handleClick }/></li>
+                <li class='md:pl-12'>
+                    <img src={require('../images/letter-b2.png')} alt="" />
+                </li>
+                <li class='md:invisible'><MenuOutlinedIcon color='success' fontSize={'large'} onClick={ handleClick }/></li>
             </div>
             <div class="hidden md:flex md:basis-1/2 items-center justify-between">
-                <li>About</li>
-                <li>Education</li>
-                <li>Projects</li>
-                <li>Contact</li>
-                <li class='border-solid border-2 rounded border-slate-600 text-slate-100 p-2'>Download Resume</li>
+                <li class='no-underline hover:underline decoration-lime-500 decoration-dotted underline-offset-8'>About</li>
+                <li class='no-underline hover:underline decoration-lime-500 decoration-dotted underline-offset-8'>Education</li>
+                <li class='no-underline hover:underline decoration-lime-500 decoration-dotted underline-offset-8'>Projects</li>
+                <li class='no-underline hover:underline decoration-lime-500 decoration-dotted underline-offset-8'>Contact</li>
+                <li class='border-solid border-2 rounded-lg border-lime-500 text-slate-100 p-2'>Download Resume</li>
             </div>
         </ul>
         
@@ -39,7 +41,7 @@ const Navigation = () => {
                 <li class='flex justify-center p-2'>Education</li>
                 <li class='flex justify-center p-2'>Projects</li>
                 <li class='flex justify-center p-2'>Contact</li>
-                <li class='flex justify-center border-solid border-2 rounded border-slate-600 text-slate-100 p-2'>Download Resume</li>
+                <li class='flex justify-center border-solid border-2 rounded border-lime-500 text-slate-100 p-2'>Download Resume</li>
             </ul>
         </div>
 
